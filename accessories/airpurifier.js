7945fdde00f5aa7813dc09e55c812eb0dce120ab
haywirecoder <44388297+haywirecoder@users.noreply.github.com>
2022-09-13 01:00:09 -0400

    Add files via upload

--- accessories/airpurifier.js
+++ accessories/airpurifier.js
@@ -1,29 +1,43 @@
 "use strict";
 
 const CLEAN_AIR_MODE = '1004';
+const CLEANAIR_ON = 1;
+const CLEANAIR_OFF = 0;
+
+const { HomeBridgeDehumidifierApplianceVersion } = require('../package.json');
 
 class airPurifierAppliance {
-    constructor(frig, device, config, log, Service, Characteristic, UUIDGen, parentAccessory) {
+    constructor(frig, deviceIndex, device, config, log, Service, Characteristic, UUIDGen, parentAccessory) {
     this.Characteristic = Characteristic;
     this.Service = Service;
     this.name = device.name + " Air Purifier";
+    this.index = deviceIndex;
     this.AirFilterMode = device.clearAirMode || 0;
     this.mode = device.mode ||0;
     this.log = log;
     this.parentAppliance = parentAccessory;
-    this.serialNumber = device.serialNumber;
-    this.firmware = device.firmwareVersion;
+    this.serialNumber = device.serialNumber; 
+    this.firmware = device.firmwareVersion || HomeBridgeDehumidifierApplianceVersion;
+  
     this.deviceid = device.deviceId.toString() + "-" +CLEAN_AIR_MODE;
     this.frig = frig;
-    this.frig.on(this.deviceid, this.refreshState.bind(this));
+    this.frig.on(device.deviceId.toString(), this.refreshState.bind(this));
     this.uuid = UUIDGen.generate(this.deviceid);
     
   }
 
   refreshState(eventData)
   {
-    this.log.debug(`Appppliance updated requested: ` , eventData);
-
+    this.log(`Appppliance updated requested: ` , eventData);
+    var airPurifierService = this.accessory.getService(this.Service.AirPurifier);
+    this.AirFilterMode = eventData.device.clearAirMode || 0;
+    this.mode = eventData.device.mode || 0;
+    // Update Characteristics to reflect new state.
+    if ((this.AirFilterMode == 1) && (this.mode != 0)) {
+      airPurifierService.updateCharacteristic(this.Characteristic.CurrentAirPurifierState,this.Characteristic.CurrentAirPurifierState.PURIFYING_AIR);
+      airPurifierService.updateCharacteristic(this.Characteristic.TargetAirPurifierState,this.Characteristic.TargetAirPurifierState.AUTO);
+    }
+    else airPurifierService.updateCharacteristic(this.Characteristic.CurrentAirPurifierState,this.Characteristic.CurrentAirPurifierState.INACTIVE);
   }
 
   setAccessory(accessory) {
@@ -31,7 +45,7 @@ class airPurifierAppliance {
     this.accessory.getService(this.Service.AccessoryInformation)
         .setCharacteristic(this.Characteristic.Manufacturer, 'Frigidaire')
         .setCharacteristic(this.Characteristic.Model, 'Dehumidifier Air Purifier')
-        .setCharacteristic(this.Characteristic.FirmwareRevision, this.version)
+        .setCharacteristic(this.Characteristic.FirmwareRevision, this.firmware)
         .setCharacteristic(this.Characteristic.SerialNumber, this.serialNumber);
     
      // create a new air purifier service
@@ -46,10 +60,18 @@ class airPurifierAppliance {
   // Handle requests to get the current value of the "Active" characteristic
   
   async getAirActive(callback) {
-    this.log('Triggered GET AirActive');
     var currentValue = this.Characteristic.Active.INACTIVE;
+    var airPurifierService = this.accessory.getService(this.Service.AirPurifier);
     // set this to a valid value for Active only if air filter mode is on and main applicaince is on.
-    if ((this.AirFilterMode == 1) && (this.mode != 0)) currentValue = this.Characteristic.Active.ACTIVE;
+    if ((this.AirFilterMode == 1) && (this.mode != 0)) {
+      airPurifierService.updateCharacteristic(this.Characteristic.CurrentAirPurifierState,this.Characteristic.CurrentAirPurifierState.PURIFYING_AIR);
+      airPurifierService.updateCharacteristic(this.Characteristic.TargetAirPurifierState,this.Characteristic.TargetAirPurifierState.AUTO);
+      currentValue = this.Characteristic.Active.ACTIVE;
+    }
+    else {
+      airPurifierService.updateCharacteristic(this.Characteristic.CurrentAirPurifierState,this.Characteristic.CurrentAirPurifierState.INACTIVE);
+
+    }
 
     return callback(null,currentValue);
   }
@@ -57,31 +79,25 @@ class airPurifierAppliance {
   // Handle requests to set the "Active" characteristic
    
   async setAirActive(value, callback) {
-    this.log('Triggered SET AirActive', value);
-   
     var airPurifierService = this.accessory.getService(this.Service.AirPurifier);
+    // Send message to parent device to activate.
+    try {
+        const returnActiveResp = await this.parentAppliance.dehumidifierAirFilterHandler(this.index,value);
+        if (returnActiveResp > 0) {
+          // Set filter state information
+          if (value == this.Characteristic.CurrentAirPurifierState.ACTIVE ) {
+            airPurifierService.updateCharacteristic(this.Characteristic.CurrentAirPurifierState,this.Characteristic.CurrentAirPurifierState.PURIFYING_AIR);
+            airPurifierService.updateCharacteristic(this.Characteristic.TargetAirPurifierState,this.Characteristic.TargetAirPurifierState.AUTO);
+          }
+          else 
+            airPurifierService.updateCharacteristic(this.Characteristic.CurrentAirPurifierState,this.Characteristic.CurrentAirPurifierState.INACTIVE);
+          this.AirFilterMode = returnActiveResp;
+        }
+      } catch(e) {
+        
+        this.log.error('Dehumidifier Air Filter encounter an error: ', err);
 
-    // var airPurifierService = this.accessory.getService(this.Service.AirPurifier);
-    // if (this.mode != 0) {
-    //   // Execute codes
-      
-    // } else {
-    //   this.log.warn('The main appliance must be ON first. Please turn ON the main appliance before activating air purifier.');
-    //   setTimeout(function () {airPurifierService.updateCharacteristic(this.Characteristic.Active,this.Characteristic.Active.INACTIVE)}.bind(this),1000);
-    // }
-    if (value == this.Characteristic.Active.ACTIVE) {
-      // Send message to parent device to activate.
-      // var dehumidifierService = this.parentAppliance.accessory.getService(this.Service.HumidifierDehumidifier);
-      // dehumidifierService.updateCharacteristic(this.Characteristic.Active,this.Characteristic.Active.ACTIVE);
-
-      this.parentAppliance.DehumidifierHandler(value);
-      // Set filter state information
-      airPurifierService.updateCharacteristic(this.Characteristic.CurrentAirPurifierState,this.Characteristic.CurrentAirPurifierState.PURIFYING_AIR);
-      airPurifierService.updateCharacteristic(this.Characteristic.TargetAirPurifierState,this.Characteristic.TargetAirPurifierState.AUTO);
-    }
-    else {
-      airPurifierService.updateCharacteristic(this.Characteristic.CurrentAirPurifierState,this.Characteristic.CurrentAirPurifierState.INACTIVE);
-    } 
+      }
     return callback(null);
   }
 
